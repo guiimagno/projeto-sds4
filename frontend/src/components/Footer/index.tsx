0545a5e983bd5eb82ab498d143d868b9c5209bc7
guilherme <guilherme.mprata@gmail.com>
2021-09-06 15:06:33 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,17 @@
+
+function Footer() {
+    return (
+
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://https://www.linkedin.com/in/guilherme-magno/" target="_blank" rel="noreferrer">Guilherme Magno</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
+
+
